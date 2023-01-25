369aac9519d8be1c0b13c4df8fbf9217bb510be9
Jorge Sevilla <jorge.garcia@compasso.com.br>
2023-01-25 15:51:24 -0300

    Estructura de carpetas

---
+++ src/routes/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <React.Fragment>
+    <div>
+      <p>App</p>
+    </div>
+    </React.Fragment>
+  );
+}
+
+export { App };
