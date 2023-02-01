399a1490b31ebfe8ad8684a47c00d63a2a98f179
Jorge Sevilla <jorge.garcia@compasso.com.br>
2023-01-31 22:49:40 -0300

    Configuración de rutas de APP

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import { EditTodoPage } from "./edit/EditTodoPage";
+import { HomePage } from "./home/HomePage";
+import { NewTodoPage } from "./new/NewTodoPage";
 
 function App() {
   return (
-    <React.Fragment>
-    <div>
-      <p>App</p>
-    </div>
-    </React.Fragment>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/new" element={<NewTodoPage />} />
+        <Route path="/edit/:id" element={<EditTodoPage />} />
+        <Route path="*" element={<p>Not Found</p>} />
+      </Routes>
+    </HashRouter>
   );
 }
 
